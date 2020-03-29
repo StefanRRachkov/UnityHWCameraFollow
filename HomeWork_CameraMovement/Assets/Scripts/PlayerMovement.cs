ca1f42ace288162bbf58df755d12a80043711755
AstrexXx <astrex17@abv.bg>
2020-03-29 14:06:54 +0300

    HomeWork Done

---
+++ HomeWork_CameraMovement/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] 
+    private float moveSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = 
+            moveSpeed 
+            * Time.deltaTime 
+            * new Vector3(Input.GetAxis("Horizontal"), 
+                    0, 
+                    Input.GetAxis("Vertical")) 
+                .normalized;
+
+        transform.position += moveDirection;
+
+    }
+}
