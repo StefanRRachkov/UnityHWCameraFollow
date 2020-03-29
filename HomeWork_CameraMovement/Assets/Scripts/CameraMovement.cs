f3445f5c732c28a9ca52a96085ea4e79eb94a6fc
AstrexXx <astrex17@abv.bg>
2020-03-29 14:23:52 +0300

    SmoothCamera

--- HomeWork_CameraMovement/Assets/Scripts/CameraMovement.cs
+++ HomeWork_CameraMovement/Assets/Scripts/CameraMovement.cs
@@ -9,6 +9,9 @@ public class CameraMovement : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+
+    [SerializeField] 
+    private float smoothSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,14 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*If you want to see smooth camera movement comment 23th line and uncomment the rest*/
         transform.position = player.transform.position + offset;
+        
+        // Thanks to Brackeys 
+        /*
+        Vector3 desiredPosition = player.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+        */
     }
 }
